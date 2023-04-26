2d413b1178e66cfc9380e5c8f87926e6bd310a70
NAGATOXD45 <112297053+NAGATOXD45@users.noreply.github.com>
2023-04-26 17:23:38 -0500

    UPDATE

---
+++ CSS/preloader.js
@@ -0,0 +1,4 @@
+// Ocultar el preloader una vez que se cargue la página
+window.addEventListener('load', function() {
+    document.getElementById('preloader').style.display = 'none';
+  });
---
+++ README.md
@@ -0,0 +1,1 @@
+# PRUEBAXD45
