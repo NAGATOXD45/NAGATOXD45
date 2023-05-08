2a4a7b1a2364ca448f9eda910c751021975abfd7
NAGATOXD45 <112297053+NAGATOXD45@users.noreply.github.com>
2023-05-08 16:37:04 -0500

    UPDATE 6

--- script.js
+++ script.js
@@ -32,30 +32,36 @@
         });
       });
 
-      $('#search1, #search2, #search3, #search4').on('keyup change', function(){
+      $('#search1, #search2, #search3, #search4, #search5, #search6').on('keyup change', function(){
         var search1 = $('#search1').val().toLowerCase();
         var search2 = $('#search2').val().toLowerCase();
         var search3 = $('#search3').val().toLowerCase();
         var search4 = $('#search4').val().toLowerCase();
+        var search5 = $('#search5').val().toLowerCase();
+        var search6 = $('#search6').val().toLowerCase();
         var filtered_rows = 0;// variable para contar filas filtradas
         $('table tbody tr').each(function(index){
           if(index === 0){ // skip first row (column headers)
             return;
           }
-
+    
           var text1 = $(this).find('td').eq(1).text().toLowerCase();
           var text2 = $(this).find('td').eq(2).text().toLowerCase();
           var text3 = $(this).find('td').eq(3).text().toLowerCase();
-          var text4 = $(this).find('td').eq(4).text().toLowerCase();
+          var text4 = $(this).find('td').eq(5).text().toLowerCase();
+          var text5 = $(this).find('td').eq(13).text().toLowerCase();
+          var text6 = $(this).find('td').eq(14).text().toLowerCase();
           var match1 = true;
           var match2 = true;
           var match3 = true;
-          var match4 = true;  
-
+          var match4 = true;
+          var match5 = true;
+          var match6 = true;
+    
           if(search1 !== ''){
             match1 = match1 && text1.indexOf(search1) !== -1;
           }
-
+    
           if(search2 !== ''){
             match2 = match2 && text2.indexOf(search2) !== -1;
           }
@@ -67,8 +73,16 @@
           if(search4 !== ''){
             match4 = match4 && text4.indexOf(search4) !== -1;
           }
-
-          if(match1 && match2 && match3 && match4){
+    
+          if(search5 !== ''){
+            match5 = match5 && text5.indexOf(search5) !== -1;
+          }
+    
+          if(search6 !== ''){
+            match6 = match6 && text6.indexOf(search6) !== -1;
+          }
+    
+          if(match1 && match2 && match3 && match4 && match5 && match6){
             $(this).show();
             filtered_rows++;//MOSTRAR CONTADOR
           } else{
@@ -76,6 +90,18 @@
           }
         });
         $('#record_count_label').text('REGISTROS: ' + filtered_rows);  
+      });
+    
+      $('#show50').click(function(){
+        $('table tbody tr').hide();
+        $('table tbody tr:lt(51)').show();
+        $('#record_count_label').text('REGISTROS: ' + ($('table tbody tr:visible').length - 1));
+      });
+
+      $('#show100').click(function(){
+        $('table tbody tr').hide();
+        $('table tbody tr:lt(101)').show();
+        $('#record_count_label').text('REGISTROS: ' + ($('table tbody tr:visible').length - 1));
       });
 
       $('#show200').click(function(){
