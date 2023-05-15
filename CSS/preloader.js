d2a4c66e8558d24ff89bf92fddffc7e57100b7a5
NAGATOXD45 <112297053+NAGATOXD45@users.noreply.github.com>
2023-05-15 10:11:28 -0500

    DASD

--- CSS/preloader.js
+++ CSS/preloader.js
@@ -1,4 +1,4 @@
 // Ocultar el preloader una vez que se cargue la página
 window.addEventListener('load', function() {
-    document.getElementById('preloader').style.display = 'none';
-  });
+document.getElementById('preloader').style.display = 'none';
+});
